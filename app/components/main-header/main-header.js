13c18ef36b0dd477e5f1475a12f7c7b4ca1f3e9e
aleksandra <aleksandra.swigon91@gmail.com>
2024-01-28 17:22:17 +0100

    nav-link

--- app/components/main-header/main-header.js
+++ app/components/main-header/main-header.js
@@ -3,6 +3,7 @@ import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
@@ -13,12 +14,13 @@ export default function MainHeader() {
           <Image src={logoImg} alt='A plate with food on in' priority></Image>
           NextLevel Food
         </Link>
-
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href='/meals'>Browse Meals</Link>
-              <Link href='/community'>Foodies Community</Link>
+              <NavLink href='/meals'>Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href='/community'> Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
