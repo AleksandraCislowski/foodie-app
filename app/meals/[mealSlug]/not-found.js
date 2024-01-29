cc22aa464d22ddad9b93e840059c4d9c8fd86190
aleksandra <aleksandra.swigon91@gmail.com>
2024-01-29 14:36:59 +0100

    another not found page

---
+++ app/meals/[mealSlug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>MEAL NOT FOUND</h1>
+      <p>Unfortunately, we could not find the requested meal.</p>
+    </main>
+  );
+}
--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,14 @@
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 import classes from "./page.module.css";
 import Image from "next/image";
 
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
 
