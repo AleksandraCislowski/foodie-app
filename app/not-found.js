d584cf7d78cae1fb0d1b58331fdd48e4f91f6897
aleksandra <aleksandra.swigon91@gmail.com>
2024-01-29 14:09:38 +0100

    error handling

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>Sorry!</h1>
+      <h1>NOT FOUND</h1>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+  );
+}
