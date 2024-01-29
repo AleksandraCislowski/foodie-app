afc2de8d3f4677c806741ef23f4e650d059a2008
aleksandra <aleksandra.swigon91@gmail.com>
2024-01-29 12:47:50 +0100

    mealitem styling

---
+++ app/components/meals/meals-grid.js
@@ -0,0 +1,11 @@
+import classes from "./meals-grid.module.css";
+
+export default function MealsGrid() {
+  return (
+    <ul className={classes.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}></li>
+      ))}
+    </ul>
+  );
+}
