052bad9474ade03fcebaccb7c355da015321c77a
aleksandra <aleksandra.swigon91@gmail.com>
2024-01-30 20:27:22 +0100

    imape picker preview

--- app/meals/share/image-picker.js
+++ app/meals/share/image-picker.js
@@ -1,18 +1,40 @@
 "use client";
+import Image from "next/image";
 import classes from "./image-picker.module.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function ImagePicker({ label, name }) {
   const imageInput = useRef();
+  const [pickedImage, setPickedImage] = useState();
 
   function handlePickClick() {
     imageInput.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      return;
+      setPickedImage(null);
+    }
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
   }
 
   return (
     <div className={classes.picker}>
       <label htmlFor='image'>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image src={pickedImage} alt='Image selected by the viewer' fill />
+          )}
+        </div>
         <input
           className={classes.input}
           type='file'
@@ -20,6 +42,7 @@ export default function ImagePicker({ label, name }) {
           accept='image/png, image/jpeg'
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
         ></input>
         <button
           className={classes.button}
